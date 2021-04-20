a0fe3360dabec0e79220dbe48a845b0d396f5903
RamyKlouz <ramy.klouz@esprit.tn>
2021-04-20 12:41:46 +0100

    fixing cnx issues

--- Proxy/connexion.cpp
+++ Proxy/connexion.cpp
@@ -1,18 +1,20 @@
 #include "connexion.h"
 #include <QSqlError>
+#include <QDebug>
 
 Connexion::Connexion(){
                       }
 bool Connexion::ouvrirConnexion()
 {bool test=false;
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-                           db.setDatabaseName("proxy");
+                           db.setDatabaseName("control");
                            db.setUserName("ramy");//inserer nom de l'utilisateur
                            db.setPassword("ramy");//inserer mot de passe de cet utilisateur
 
-if (db.open())
+if (db.open()){
     test=true;
-
+    qDebug() << "base t7allet";
+}
 else throw QString ("Erreur Paramétres"+db.lastError().text());
 return  test;
 }
